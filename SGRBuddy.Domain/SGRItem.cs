5b655ded69c67e917511c1ccb4c209b2a57b2b17
Radu Diaconu <radudiaconu240@gmail.com>
2026-06-05 18:48:32 +0300

    Made Domain

---
+++ SGRBuddy.Domain/SGRItem.cs
@@ -0,0 +1,12 @@
+﻿namespace SGRBuddy.Domain;
+
+public class SGRItem
+{
+    public Guid Id { get; set; }
+    public string Barcode  { get; set; }
+    public string Brand { get; set; }
+    public decimal Capacity { get; set; }
+    public bool IsAlcohol { get; set; }
+    public decimal Price { get; set; }
+    public int Count { get; set; }
+}
---
+++ SGRBuddy.Domain/SGRSession.cs
@@ -0,0 +1,14 @@
+namespace SGRBuddy.Domain;
+
+public class SGRSession
+{
+    public Guid Id { get; set; }
+    
+    public DateTime StartDate { get; set; }
+    
+    public DateTime EndDate { get; set; }
+    
+    public ICollection<SGRItem> SGRItems { get; set; }
+    
+    public decimal TotalPrice { get; set; }
+}
